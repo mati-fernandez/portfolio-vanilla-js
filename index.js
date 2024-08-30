e99b2bfb3290a3e937da61433ae2e35c7493f1fc
Matias Fernandez <matifer.dev@gmail.com>
2024-08-30 01:16:44 -0300

    Different size for different aspect ratio on habilidades icons implemented with JS

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 //Establecer todo el js después de la carga del dom:
 document.addEventListener('DOMContentLoaded', (e) => {
-  const $scrollToBottom = document.querySelector('#scroll-to-bottom')
+  const $scrollToBottom = document.querySelector('#scroll-to-bottom');
 
   //Variables y constantes de uso global:
-  const wideVersionMinWidth = 800
-  let wideVersion = window.innerWidth > wideVersionMinWidth ? true : false
+  const wideVersionMinWidth = 800; // FALTA CORREGIR, VERI PROP WINDOW.ASPECTRATIO SI HAY
+  let wideVersion = window.innerWidth > wideVersionMinWidth ? true : false;
 
-  /************************* FUNCIONES ******************************/
+  // Asignación de aspect ratio a imágenes de habilidades
+  document.querySelectorAll('.skills-list img').forEach((img) => {
+    if (img.naturalWidth > img.naturalHeight) {
+      img.classList.add('landscape');
+    } else {
+      img.classList.add('portrait');
+    }
+  });
 
   //Manejo de eventos click
   document.addEventListener('click', (e) => {
     //Scroll to top general
     if (e.target.matches('#scroll-to-top')) {
-      $scrollToBottom.style.display = 'block'
+      $scrollToBottom.style.display = 'block';
       window.scrollTo({
         behavior: 'smooth',
         top: 0,
-      })
+      });
     }
     //Scroll to bottom general
     if (e.target.matches('#scroll-to-bottom')) {
       window.scrollTo({
         behavior: 'smooth',
         top: 50000,
-      })
+      });
     }
-  })
+  });
 
   //Scroll to sections
   document.querySelectorAll('.fa-chevron-down').forEach((button, index) => {
     button.addEventListener('click', () => {
-      const sectionId = index === 0 ? '#seccion-skills' : '#seccion-proyectos'
+      const sectionId = index === 0 ? '#seccion-skills' : '#seccion-proyectos';
       document.querySelector(sectionId).scrollIntoView({
         behavior: 'smooth',
         block: 'start',
-      })
-    })
-  })
+      });
+    });
+  });
 
   //Manejo de eventos hover
   document.addEventListener('mouseover', (e) => {
@@ -44,47 +51,47 @@ document.addEventListener('DOMContentLoaded', (e) => {
     // DESKTOP
     if (window.innerWidth > wideVersionMinWidth) {
       if (e.target.matches('#prof-pic-area')) {
-        console.log('Mouseover PC')
+        console.log('Mouseover PC');
         // MOBILE
       } else {
         if (e.target.matches('#caja-cara')) {
-          console.log('Mouseover MOBILE')
+          console.log('Mouseover MOBILE');
         }
       }
     }
-  })
+  });
 
   //Manejo de eventos mouseout
   document.addEventListener('mouseout', (e) => {
     //Manejo del mouseout en profile pic
     if (window.innerWidth > wideVersionMinWidth) {
       if (e.target.matches('#prof-pic-area')) {
-        console.log('mouseout PC')
+        console.log('mouseout PC');
       }
     } else {
       if (e.target.matches('#caja-cara')) {
-        console.log('mouseout MOBILE')
+        console.log('mouseout MOBILE');
       }
     }
-  })
+  });
 
   //Long press en mobile, click derecho en pc
   document.addEventListener('contextmenu', function (event) {
-    event.preventDefault()
+    event.preventDefault();
     //Click derecho en profile pic
     if (event.target.matches('img#profile-pic')) {
       // Agregar algun efecto quizas
     }
-  })
+  });
 
   window.addEventListener('resize', (e) => {
     // console.log('Resize event, window innerWidth', e.target.innerWidth);
     if (e.target.innerWidth > wideVersionMinWidth && !wideVersion) {
-      wideVersion = true
-      console.log('Wide version', wideVersion)
+      wideVersion = true;
+      console.log('Wide version', wideVersion);
     } else if (e.target.innerWidth <= wideVersionMinWidth && wideVersion) {
-      wideVersion = false
-      console.log('Wide version', wideVersion)
+      wideVersion = false;
+      console.log('Wide version', wideVersion);
     }
-  })
-})
+  });
+});
